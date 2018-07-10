c830e5b0637da1f0171807dbfb89692554ab7967
Roberto Gaitan <rgaitan.it@gmail.com>
2018-07-10 19:26:12 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -23,8 +23,13 @@ def how_many_steps?
   steps = 0
   loop do
     puts "#{steps}"
+    if steps % 2 == 0
+      puts "Left"
+    else
+      puts "Right"
+    end
     steps += 1
-
+    sleep(0.5) 
   end
 end
 
